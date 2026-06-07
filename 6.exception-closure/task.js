f360a1bb1827a8f02c73cad36c2078ea899c6861
Danya <daniil-baydik@mail.ru>
2026-06-07 21:33:37 +0300

    Сделал 6 практическое задание, было классно

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(value) {  
+  let parsed = Number.parseFloat(value);
+  if (isNaN(parsed)) {
+    throw new Error("Невалидное значение");
+  } else {
+    return parsed;
+  }
+}
+
+function validateCount(value) {
+  try {
+    let result = parseCount(value);
+    return result;
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this._a = a;
+    this._b = b;
+    this._c = c;
+  }
+  
+  get perimeter() {
+    return this._a + this._b + this._c;
+  }
+  
+  get area() {
+    let p = this.perimeter / 2;
+    const area = Math.sqrt(p * (p - this._a) * (p - this._b) * (p - this._c));
+    return Math.round(area * 1000) / 1000;
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      }
+    };
+  }
+}
